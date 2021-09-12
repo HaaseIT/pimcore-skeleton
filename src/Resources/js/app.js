471a6d9b062eed6c6792f59730a809ab90012f47
Marcus Haase <mail@marcus.haase.name>
2021-09-12 09:09:38 +0200

    commented out some stuff that required values before use

--- src/Resources/js/app.js
+++ src/Resources/js/app.js
@@ -2,29 +2,29 @@ require('../scss/app.scss');
 
 require('bootstrap');
 require('@popperjs/core');
-require("cookieconsent");
+// require("cookieconsent");
 
 const xhrDONE = 4; // readyState 4 means the request is done.
 const xhrOK = 200; // status 200 is a successful return.
 
-const gsap = require("gsap/all");
+// const gsap = require("gsap/all");
 
 document.addEventListener("DOMContentLoaded", function () {
-
-    cookieconsent.initialise(
-        {
-            "palette": {
-                "popup": {
-                    "background": "#fff",
-                    "text": "#000"
-                },
-                "button": {
-                    "background": document.cookeButtonColor,
-                    "text": "#ffffff"
-                }
-            },
-            "position": "bottom-right",
-            "content": document.cookietext,
-        }
-    );
+    // TODO: enable if cookieconsent is required
+    // cookieconsent.initialise(
+    //     {
+    //         "palette": {
+    //             "popup": {
+    //                 "background": "#fff",
+    //                 "text": "#000"
+    //             },
+    //             "button": {
+    //                 "background": document.cookeButtonColor,
+    //                 "text": "#ffffff"
+    //             }
+    //         },
+    //         "position": "bottom-right",
+    //         "content": document.cookietext,
+    //     }
+    // );
 });
