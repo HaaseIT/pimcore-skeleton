88027db985c229871034898ef02f8fcee00368b0
Marcus Haase <mail@marcus.haase.name>
2021-11-13 14:34:21 +0100

    better cookieconstent code

--- src/Resources/js/app.js
+++ src/Resources/js/app.js
@@ -4,6 +4,8 @@ require('bootstrap');
 require('@popperjs/core');
 // require("cookieconsent");
 
+//const elementConfiguration = document.getElementById("pageConfiguration");
+
 const xhrDONE = 4; // readyState 4 means the request is done.
 const xhrOK = 200; // status 200 is a successful return.
 
@@ -13,18 +15,36 @@ document.addEventListener("DOMContentLoaded", function () {
     // TODO: enable if cookieconsent is required
     // cookieconsent.initialise(
     //     {
-    //         "palette": {
-    //             "popup": {
-    //                 "background": "#fff",
-    //                 "text": "#000"
+    //         revokable: true,
+    //         palette: {
+    //             popup: {
+    //                 background: "#fff",
+    //                 text: "#05509D"
     //             },
     //             "button": {
-    //                 "background": document.cookeButtonColor,
+    //                 "background": "#162c5d",
     //                 "text": "#ffffff"
     //             }
     //         },
-    //         "position": "bottom-right",
-    //         "content": document.cookietext,
+    //         position: "bottom-right",
+    //         type: "opt-in",
+    //         content: {
+    //             message: elementConfiguration.dataset.cc_message,
+    //             link: elementConfiguration.dataset.cc_link,
+    //             dismiss: elementConfiguration.dataset.cc_dismiss,
+    //             href: elementConfiguration.dataset.cc_href,
+    //             target: "_self",
+    //             allow: elementConfiguration.dataset.cc_allow,
+    //             deny: elementConfiguration.dataset.cc_deny,
+    //             policy: elementConfiguration.dataset.cc_policy,
+    //         },
+    //         law: {
+    //             regionalLaw: true,
+    //             countryCode: "DE",
+    //         },
+    //         onStatusChange: function (status) {
+    //             location.reload();
+    //         },
     //     }
     // );
 });
