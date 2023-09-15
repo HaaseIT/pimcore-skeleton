61e2c8571d5fe3108f43deae175a5110272c447d
Marcus Haase <mail@marcus.haase.name>
2023-09-15 13:58:44 +0200

    Added tinymcebundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Pimcore\Bundle\SystemInfoBundle\PimcoreSystemInfoBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
 ];
