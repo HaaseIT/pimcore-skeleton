a6b428b89915b49998070bf98c8f941799ed83c9
Marcus Haase <mail@marcus.haase.name>
2026-04-11 10:59:22 +0200

    studio ui

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,6 @@ return [
     Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
     Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
     Pimcore\Bundle\StaticRoutesBundle\PimcoreStaticRoutesBundle::class => ['all' => true],
-    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
     Pimcore\Bundle\XliffBundle\PimcoreXliffBundle::class => ['all' => true],
-    Pimcore\Bundle\SystemInfoBundle\PimcoreSystemInfoBundle::class => ['all' => true],
+    Pimcore\Bundle\StudioUiBundle\PimcoreStudioUiBundle::class => ['all' => true],
 ];
